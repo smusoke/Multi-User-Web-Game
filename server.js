080a0ac5af86e9359b13ff6f5d084a9839e65322
Steffan <muso9194@stthomas.edu>
2019-05-06 12:02:05 -0500

    Added game Database and login submit

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express')
 const fs = require('fs');
 const path = require('path')
-const app = express()
-const port = 3000
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser')
 
+
+const app = express()
+const port = 8019
+
+
+var db_filename = path.join(__dirname, 'db', 'gameDb.sqlite3');
+var public_dir = path.join(__dirname, 'public');
+
+app.use(express.static(public_dir));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+//Establish db
+var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+        console.log('Error opening ' + db_filename);
+    }
+    else {
+        console.log('Now connected to ' + db_filename);
+    }
+});
+
+//Index page
 app.get('/', (req, res) => {
 
 	fs.readFile("index.html", function (error, pgResp) {
@@ -18,6 +42,16 @@ app.get('/', (req, res) => {
 	            res.end();
 	        });
 });
+
+
+app.post('/login', function (req, res) {
+    //res.send('POST Request');
+    //console.log(res);
+    console.log(req.body.username);
+    console.log(req.body.password);
+});
+
+
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}!`)
